0f24866bc0f39a2b56779e11c9081b6ad407ee15
Avutzhan <dautov92@list.ru>
2020-02-08 15:43:03 +0600

    Migrations
    Added new migrations in /database/migrations
    
    -create_corrals_table
    -create_sheeps_table
    -create_activity_log_table
    -create_dates_table

---
+++ database/migrations/2020_02_08_092412_create_sheeps_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSheepsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sheeps', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('date');
+            $table->bigInteger('corral_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('corral_id')
+                ->references('id')
+                ->on('corrals')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection(config('activitylog.database_connection'))->dropIfExists(config('activitylog.table_name'));
+    }
+}
