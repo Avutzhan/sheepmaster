294562f264a5b37f47ac056a9d9bcb0f4207f434
Avutzhan <dautov92@list.ru>
2020-02-09 00:30:28 +0600

    Reports Api
    Added ReportController with index and show methods. Also added routes for those methods in routes/api.
    
    -Routes
    -ReportController

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Date;
+
+class ReportController extends Controller
+{
+    public function index(){
+        $reports = Date::orderBy('id', 'desc')->get()->groupBy(function($item){return $item->date;});
+
+        return $reports;
+    }
+
+    public function show($key){
+        $reports = Date::where('date',$key)->orderBy('id', 'desc')->get();
+        $created = Date::where('date',$key)->whereRaw("description LIKE '%created%'")->count();
+        $updated = Date::where('date',$key)->whereRaw("description LIKE '%updated%'")->count();
+        $deleted = Date::where('date',$key)->whereRaw("description LIKE '%deleted%'")->count();
+        return [$reports, $created, $updated, $deleted];
+    }
+}
